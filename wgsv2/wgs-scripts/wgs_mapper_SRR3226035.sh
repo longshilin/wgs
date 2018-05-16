e80a90f26617505869671046bb3484c9a15a019a
elon <yilong0722@gmail.com>
2018-05-16 17:30:15 +0800

    Rebuild project wgsv3 for hadoop cluster

--- wgsv2/wgs-scripts/wgs_mapper_SRR3226035.sh
+++ wgsv2/wgs-scripts/wgs_mapper_SRR3226035.sh
@@ -7,7 +7,8 @@
 # 脚本所需的环境变量定义
 export BWA=~/biosoft/bwa/0.7.12
 export SAMTOOLS=~/biosoft/samtools/1.0/bin
-export GATK=~/biosoft/gatk/4.0/gatk
+export GATK=~/biosoft/gatk/3.6/GenomeAnalysisTK.jar
+export PICARD=~/biosoft/picard/2.18.2/picard.jar
 export HADOOP=~/hadoop/bin
 
 # 定义参考序列
@@ -44,33 +45,23 @@ echo "*** 从HDFS上下载样本数据块" && echo "" >&2
 # ××××××  基因测序 ×××××××
 # 步骤一 比对
 echo "###COMMAND LINE###：" >&2
-echo "$BWA/bwa mem -t $THREAD -R $INFO_RG $REF $INPUT_FILE_R1 $INPUT_FILE_R2 | \
-$SAMTOOLS/samtools view -Sb - > SRR3226035.bam" >&2
+echo "$BWA/bwa mem -t $THREAD -R $INFO_RG $REF $INPUT_FILE_R1 $INPUT_FILE_R2 | $SAMTOOLS/samtools view -Sb - > SRR3226035.bam" >&2
 
-time $BWA/bwa mem -t $THREAD -R $INFO_RG $REF $INPUT_FILE_R1 $INPUT_FILE_R2 | \
-$SAMTOOLS/samtools view -Sb - > SRR3226035.bam && echo "*** 基因比对操作完成" && echo "" >&2
-
-rm -f $INPUT_FILE_R1 $INPUT_FILE_R2
+time $BWA/bwa mem -t $THREAD -R $INFO_RG $REF $INPUT_FILE_R1 $INPUT_FILE_R2 | $SAMTOOLS/samtools view -Sb - > SRR3226035.bam && echo "*** 基因比对操作完成" && echo "" >&2
 
 # 步骤二 排序
 echo "###COMMAND LINE###：" >&2
-echo "$SAMTOOLS/samtools sort -@ $THREAD -m $MEMORY -O bam -o SRR3226035.sorted.bam \
-SRR3226035.bam -T PREFIX.bam" >&2
+echo "$SAMTOOLS/samtools sort -@ $THREAD -m $MEMORY -O bam -o SRR3226035.sorted.bam SRR3226035.bam -T PREFIX.bam" >&2
 
-time $SAMTOOLS/samtools sort -@ $THREAD -m $MEMORY -O bam -o SRR3226035.sorted.bam \
-SRR3226035.bam -T PREFIX.bam && echo "*** 基因数据排序完成" && echo "" >&2
+time $SAMTOOLS/samtools sort -@ $THREAD -m $MEMORY -O bam -o SRR3226035.sorted.bam SRR3226035.bam -T PREFIX.bam && echo "*** 基因数据排序完成" && echo "" >&2
 
-rm -f SRR3226035.bam
+#rm -f SRR3226035.bam
 
 # 步骤三 标记重复
 echo "###COMMAND LINE###：" >&2
-echo "$GATK MarkDuplicates -I SRR3226035.sorted.bam -O SRR3226035.sorted.markdup.bam \
--M SRR3226035.sorted.markup_metrics.txt" >&2
+echo "java -jar $PICARD MarkDuplicates I=SRR3226035.sorted.bam O=SRR3226035.sorted.markdup.bam M=SRR3226035.sorted.markdup_metrics.txt" >&2
 
-time $GATK MarkDuplicates -I SRR3226035.sorted.bam -O SRR3226035.sorted.markdup.bam \
--M SRR3226035.sorted.markdup_metrics.txt 1>&2 && echo "*** 对BAM文件进行重复标记" && echo "" >&2
-
-rm -f SRR3226035.sorted.bam
+time java -jar $PICARD MarkDuplicates I=SRR3226035.sorted.bam O=SRR3226035.sorted.markdup.bam M=SRR3226035.sorted.markdup_metrics.txt && echo "*** 对BAM文件进行重复标记" && echo "" >&2
 
 # 步骤四 创建比对索引文件
 echo "###COMMAND LINE###：" >&2
@@ -80,13 +71,9 @@ time $SAMTOOLS/samtools index SRR3226035.sorted.markdup.bam && echo "*** 对BAM�
 
 # 步骤五 变异检测 | 生成各个样本的中间变异检测文件gvcf
 echo "###COMMAND LINE###：" >&2
-echo "$GATK HaplotypeCaller -R $REF --emit-ref-confidence GVCF -I SRR3226035.sorted.markdup.bam \
--O SRR3226035.g.vcf" >&2
+echo "java -jar $GATK -T HaplotypeCaller -R $REF --emitRefConfidence GVCF -I SRR3226035.sorted.markdup.bam -o SRR3226035.g.vcf" >&2
 
-time $GATK HaplotypeCaller -R $REF --emit-ref-confidence GVCF -I SRR3226035.sorted.markdup.bam \
--O SRR3226035.g.vcf && echo "*** 生成样本SRR3226035的中间变异检测文件gvcf" && echo "" >&2
-
-#rm -f SRR3226035.sorted.markdup.bam* SRR3226035.sorted.markdup_metrics.txt
+time java -jar $GATK -T HaplotypeCaller -R $REF --emitRefConfidence GVCF -I SRR3226035.sorted.markdup.bam -o SRR3226035.g.vcf && echo "*** 生成样本SRR3226035的中间变异检测文件gvcf" && echo "" >&2
 
 # 将各个样本分区的变异检测gvcf文件上传到HDFS上
 echo "###COMMAND LINE###：" >&2
@@ -94,5 +81,7 @@ echo "$HADOOP/hadoop fs -put -f SRR3226035.g.vcf /wgsv2/output/gvcf" >&2
 
 time $HADOOP/hadoop fs -put -f SRR3226035.g.vcf /wgsv2/output/gvcf && echo "*** 变异检测结果gvcf文件上传到HDFS上" && echo "">&2
 
-echo "--- 结束处理样本 SRR3226035 ---" && echo "" && echo ""
+#rm -f rm -f $INPUT_FILE_R1 $INPUT_FILE_R2 SRR3226035*
+
+echo "--- 结束处理样本 SRR3226035 ---"
 echo "############" `date "+%Y-%m-%d %H:%M:%S"` "############" && echo ""
